669ef97e7832afae402cf1bac2932539756cea4d
Abiodun Thorpe <abiodun.thorpe@gmail.com>
2013-06-17 13:43:47 +0100

    Web Tracking Installation
    Adding first set of tracking with Google Analytics

--- views/header.php
+++ views/header.php
@@ -14,5 +14,19 @@ var SITEURL = "<?php echo SITEURL; ?>";
 <script type="text/javascript" src="<?php echo SITEURL;?>/assets/fancybox/jquery.fancybox.pack.js"></script>
 <script type="text/javascript" src="<?php echo SITEURL;?>/assets/fancybox/helpers/jquery.fancybox-media.js"></script>
 <link rel="stylesheet" type="text/css" href="<?php echo SITEURL;?>/assets/fancybox/jquery.fancybox.css" media="screen" />
+
+<!-- Google Analytics -->
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-41775754-1', 'lifebankapp.com');
+  ga('send', 'pageview');
+
+</script>	
+<!-- End of Google Analytics -->
+
 </head>
  
--- views/hospital/header.php
+++ views/hospital/header.php
@@ -6,5 +6,19 @@ var SITEURL = "<?php echo SITEURL; ?>";
 
 <script type="text/javascript" src="<?php echo SITEURL;?>/assets/js/jquery.js"></script>
 <script type="text/javascript" src="<?php echo SITEURL;?>/assets/js/typeahead.min.js"></script>
+
+<!-- Google Analytics -->
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-41775754-1', 'lifebankapp.com');
+  ga('send', 'pageview');
+
+</script>	
+<!-- End of Google Analytics -->
+
 </head>
  
